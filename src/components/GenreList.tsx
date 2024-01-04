36dd8ef54f9e959b1ada9feb0cfaf2fb42724f54
Kapil <kingranikapil3@gmail.com>
2024-01-04 11:38:08 +0530

    Show A Spinner While Fetching A Genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import useGenres from "../hooks/useGenres";
-import { list } from "@chakra-ui/react";
+import { Spinner, list } from "@chakra-ui/react";
 
 const GenreList = () => {
-  const { genres } = useGenres();
+  const { genres, isLoading, error } = useGenres();
+
+  if (error) return null;
+  if (isLoading) return <Spinner padding={10} />;
   return (
     <ul>
       {genres.map((genre) => (
